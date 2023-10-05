c8663aec1485fb12ba89eb1c2241923478e99a19
Alex Pl <qq311565@gmail.com>
2023-10-05 21:48:11 +0300

    Generate 1s of silent

--- src/Utils/GenerateSilentChunk.ts
+++ src/Utils/GenerateSilentChunk.ts
@@ -2,7 +2,7 @@ import { AudioSampleRate } from "../Types/AudioTypes";
 
 
 const generateSilentChunk = (sampleRate: AudioSampleRate, channels: number, size: number | null) => {
-    const chunkSize: number = size ?? sampleRate * channels;
+    const chunkSize: number = size ?? (sampleRate * channels) * 2;
     const silentChunk: Buffer = Buffer.alloc(chunkSize);
 
     return silentChunk;
