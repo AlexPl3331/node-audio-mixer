1c7d5c58b33701b2c1c8a78e0c4ee3f2eab3df57
Alex Pl <qq311565@gmail.com>
2024-05-02 13:28:57 +0300

    Use linear interpolation

--- src/Utils/AudioUtils/СhangeSampleRate.ts
+++ src/Utils/AudioUtils/СhangeSampleRate.ts
@@ -12,19 +12,32 @@ export function changeSampleRate(audioData: ModifiedDataView, inputParams: Audio
 	const inputEndianness = inputParams.endianness ?? endianness();
 	const mixerEndianness = mixerParams.endianness ?? endianness();
 
-	const dataSize = Math.round((audioData.byteLength / bytesPerElement) * (mixerParams.sampleRate / inputParams.sampleRate));
+	const isDownsample = inputParams.sampleRate > mixerParams.sampleRate;
 
-	const allocData = new Int8Array(dataSize * bytesPerElement);
+	const scaleFactor = isDownsample
+		? inputParams.sampleRate / mixerParams.sampleRate
+		: mixerParams.sampleRate / inputParams.sampleRate;
+
+	const dataSize = Math.floor(audioData.byteLength * (mixerParams.sampleRate / inputParams.sampleRate));
+
+	const allocData = new Int8Array(dataSize);
 	const allocDataView = new ModifiedDataView(allocData.buffer);
 
 	const getSampleMethod: `getInt${BitDepth}` = `get${getMethodName(inputParams.bitDepth)}`;
 	const setSampleMethod: `setInt${BitDepth}` = `set${getMethodName(mixerParams.bitDepth)}`;
 
-	for (let index = 0; index < dataSize; index++) {
-		const oldSamplePosition = Math.floor(index * inputParams.sampleRate / mixerParams.sampleRate) * bytesPerElement;
-		const sample = audioData[getSampleMethod](oldSamplePosition, inputEndianness === 'LE');
+	for (let index = 0; index < dataSize / bytesPerElement; index++) {
+		const interpolatePosition = isDownsample ? index * scaleFactor : index / scaleFactor;
 
-		allocDataView[setSampleMethod](index * bytesPerElement, sample, mixerEndianness === 'LE');
+		const previousPosition = Math.floor(interpolatePosition);
+		const nextPosition = previousPosition + 1;
+
+		const previousSample = audioData[getSampleMethod](previousPosition * bytesPerElement, inputEndianness === 'LE');
+		const nextSample = audioData[getSampleMethod](nextPosition * bytesPerElement, inputEndianness === 'LE');
+
+		const interpolatedValue = ((interpolatePosition - previousPosition) * ((nextSample - previousSample) / (nextPosition - previousPosition))) + previousSample;
+
+		allocDataView[setSampleMethod](index * bytesPerElement, interpolatedValue, mixerEndianness === 'LE');
 	}
 
 	return allocDataView;
