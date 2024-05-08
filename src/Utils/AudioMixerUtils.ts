3df357220c6668e22ec6e6e8dbc379e486c5ff67
Alex Pl <qq311565@gmail.com>
2024-05-08 14:05:44 +0300

    Complete AudioMixerUtils

--- src/Utils/AudioMixerUtils.ts
+++ src/Utils/AudioMixerUtils.ts
@@ -1,34 +1,77 @@
+import {type BitDepth} from '../Types/AudioTypes';
 import {type AudioUtils} from '../Types/AudioUtils';
 import {type AudioMixerParams} from '../Types/ParamsTypes';
 
-import {assertVolume} from '../Asserts/AssertVolume';
 import {changeVolume} from './AudioUtils/СhangeVolume';
+import {assertVolume} from '../Asserts/AssertVolume';
+
+import {ModifiedDataView} from '../ModifiedDataView/ModifiedDataView';
+import {isLittleEndian} from './General/IsLittleEndian';
+import {getMethodName} from './General/GetMethodName';
 
 export class AudioMixerUtils implements AudioUtils {
 	private readonly audioMixerParams: AudioMixerParams;
+	private changedParams: AudioMixerParams;
 
-	private audioData: DataView[] = [];
+	private dataCollection: ModifiedDataView[] = [];
 
-	constructor(params: AudioMixerParams) {
-		this.audioMixerParams = params;
+	private readonly emptyData = new Int8Array(0);
+	private mixedData: ModifiedDataView;
+
+	constructor(mixerParams: AudioMixerParams) {
+		this.audioMixerParams = mixerParams;
+
+		this.changedParams = {...this.audioMixerParams};
+
+		this.mixedData = new ModifiedDataView(this.emptyData.buffer);
 	}
 
 	public setAudioData(audioData: Int8Array[]): this {
-		this.audioData = audioData.map((audioData: Int8Array) => new DataView(audioData.buffer));
+		this.dataCollection = audioData.map((audioData: Int8Array) => new ModifiedDataView(audioData.buffer));
+
+		this.changedParams = {...this.audioMixerParams};
+
+		return this;
+	}
+
+	public mixAudioData(): this {
+		const bytesPerElement = this.changedParams.bitDepth / 8;
+
+		const maxValue = (2 ** (this.changedParams.bitDepth - 1)) - 1;
+		const minValue = -(maxValue + 1);
+
+		const isLe = isLittleEndian(this.changedParams.endianness);
+
+		const audioData = new Int8Array(this.dataCollection[0].byteLength);
+		this.mixedData = new ModifiedDataView(audioData.buffer);
+
+		const getSampleMethod: `getInt${BitDepth}` = `get${getMethodName(this.changedParams.bitDepth)}`;
+		const setSampleMethod: `setInt${BitDepth}` = `set${getMethodName(this.changedParams.bitDepth)}`;
+
+		for (let index = 0; index < audioData.length; index += bytesPerElement) {
+			const samples = this.dataCollection.map(data => data[getSampleMethod](index, isLe));
+			const mixedSample = samples.reduce((sample, nextSample) => sample + nextSample, 0);
+			const clipSample = Math.min(Math.max(mixedSample, minValue), maxValue);
+
+			this.mixedData[setSampleMethod](index, clipSample, isLe);
+		}
+
 		return this;
 	}
 
 	public checkVolume(): this {
-		if (this.audioMixerParams.volume && this.audioMixerParams.volume !== 100) {
-			assertVolume(this.audioMixerParams.volume);
+		const volume = this.audioMixerParams.volume ?? 100;
 
-			// ChangeVolume(this.audioData, this.audioMixerParams);
+		if (volume > 100) {
+			assertVolume(volume);
+
+			changeVolume(this.mixedData, this.changedParams);
 		}
 
 		return this;
 	}
 
 	public getAudioData(): Int8Array {
-		return new Int8Array(0);
+		return new Int8Array(this.mixedData.buffer);
 	}
 }
