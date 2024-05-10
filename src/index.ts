29ce49bd34928db20cbb8615357ec482dab15242
Alex Pl <qq311565@gmail.com>
2024-05-10 13:48:48 +0300

    Add import from index

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import {AudioMixer} from './AudioMixer/AudioMixer';
+import {AudioInput} from './AudioInput/AudioInput';
+
+import {type SampleRate, type BitDepth, type Endianness} from './Types/AudioTypes';
+
+export {
+	AudioMixer, AudioInput, type SampleRate, type BitDepth, type Endianness,
+};
